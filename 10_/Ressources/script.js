3295f48f8e339d1b8a37647b48742778c3dd6c68
mimazouz <mimazouz@student.42.fr>
2018-03-15 15:25:44 +0100

    WIP

---
+++ 10_/Ressources/script.js
@@ -0,0 +1,31 @@
+const axios = require('ax')
+// const page = require('webpage').create()
+// const allPath = []
+
+// function getAllPath (_page) {
+//   return _page.evaluate(function() {
+//     return [].map.call(document.querySelectorAll('a'), function(link) {
+//     //  return link.getAttribute('href')
+//       const linkStr = link.getAttribute('href')
+      
+//       if (linkStr !== 'README') {
+//         console.log('87')
+//           // return _page.open(`${page.url}${linkStr}`, function() {
+//           // getAllPath(_page)
+//         // })
+//       }
+//       console.log('87')
+//       return 'toto'
+//     })
+//   })
+// }
+
+// page.open('http://172.16.106.131/.hidden/', function(status) {
+//   console.log("Status: " + status)
+//   if(status !== "success") phantom.exit(1)
+
+//   const result = getAllPath(page)
+//   console.log(result.join('\n'))
+//   phantom.exit(0)
+// })
+
